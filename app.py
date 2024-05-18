3dbe44703a43f6781ed54cd27a049316cf7288d7
joshva2104 <49963261+joshva2104@users.noreply.github.com>
2024-05-18 13:09:44 +0530

    added model for diabetes distress

--- app.py
+++ app.py
@@ -34,9 +34,7 @@ loaded_model = joblib.load(model_filename)
 loaded_tfidf_vectorizer = joblib.load(tfidf_vectorizer_filename)
 class_labels = loaded_model.classes_
 
-tepeyac_model = joblib.load('tepeyac_svc_model.pkl')
-tepeyac_tfidf_vectorizer = joblib.load('tepeyac_tfidf_vectorizer.pkl')
-tepeyac_labels = tepeyac_model.classes_
+
 
 app = FastAPI()
 
@@ -50,6 +48,10 @@ def c4hprediction(data: Details):
     top_class_labels = [class_labels[i] for i in top_class_indices]
     return {'message': data,'prediction': top_class_labels}
 
+tepeyac_model = joblib.load('tepeyac_svc_model.pkl')
+tepeyac_tfidf_vectorizer = joblib.load('tepeyac_tfidf_vectorizer.pkl')
+tepeyac_labels = tepeyac_model.classes_
+
 @app.post('/tepeyacprediction')
 def tepeyacprediction(data: Details):
     new_text = [data.text_to_classify]
@@ -57,6 +59,19 @@ def tepeyacprediction(data: Details):
     class_probabilities = tepeyac_model.predict_proba(new_text)
     top_class_indices = class_probabilities.argsort()[0][::-1][:4]
     top_class_labels = [tepeyac_labels[i] for i in top_class_indices]
+    return {'message': data,'prediction': top_class_labels}
+
+diabetes_distress_model = joblib.load('diabetes_distress_svc_model.pkl')
+diabetes_distress_tfidf_vectorizer = joblib.load('diabetes_distress_tfidf_vectorizer.pkl')
+diabetes_distress_labels = diabetes_distress_model.classes_
+
+@app.post('/diabetes_distress_prediction')
+def diabetes_distress_prediction(data: Details):
+    new_text = [data.text_to_classify]
+    print(new_text)
+    class_probabilities = diabetes_distress_model.predict_proba(new_text)
+    top_class_indices = class_probabilities.argsort()[0][::-1][:4]
+    top_class_labels = [diabetes_distress_labels[i] for i in top_class_indices]
     return {'message': data,'prediction': top_class_labels}
 
 if __name__ == "__main__":
